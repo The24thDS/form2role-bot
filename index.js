a8cb0e8e0a05de4ddcb200c4a3e17f97f2b9b370
The24thDS <davidsima@windowslive.com>
2019-12-10 21:33:37 +0200

    Version 1.0.0

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const Discord = require("discord.js");
 const { google } = require("googleapis");
 
 const { apiKey, spreadsheetId, range } = require("./config/google.json");
+const connection = google.sheets({
+  version: "v4",
+  auth: apiKey
+});
+
 const { token, roles } = require("./config/discord.json");
 const client = new Discord.Client();
+
+let oldRows = [];
 
 const fetchRows = async (spreadsheetId, range, sheetsConnection) => {
   try {
@@ -18,11 +25,57 @@ const fetchRows = async (spreadsheetId, range, sheetsConnection) => {
   }
 };
 
-connection = google.sheets({
-  version: "v4",
-  auth: apiKey
+const assignRoles = async (usernames, roleNames) => {
+  const guildMembers = client.guilds.array()[0].members.array();
+  const guildRoles = client.guilds.array()[0].roles.array();
+  // * getting Role instances of role names
+  const Roles = guildRoles.filter(Role => roleNames.includes(Role.name));
+  // * getting GuildMember instances of usernames and setting roles
+  guildMembers.forEach(async member => {
+    const username = member.user.username + "#" + member.user.discriminator;
+    if (usernames.includes(username)) {
+      try {
+        await member.addRoles(Roles);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  });
+};
+
+const extractNewEntries = (oldRows, rows) => {
+  let newRows = [];
+  if (rows.length > oldRows.length) {
+    newRows = rows.slice(oldRows.length);
+  }
+  newRows.forEach(row => {
+    oldRows.push(row);
+  });
+  return newRows;
+};
+
+const extractDiscordIDs = rows => {
+  return rows.map(user => user[0]);
+};
+
+client.once("ready", () => {
+  setInterval(async () => {
+    const rows = await fetchRows(spreadsheetId, range, connection);
+    const newEntries = extractNewEntries(oldRows, rows);
+    if (newEntries.length > 0) {
+      const usernames = extractDiscordIDs(newEntries);
+      assignRoles(usernames, roles);
+    } else {
+      console.log("No new entries");
+    }
+  }, 60000);
 });
 
-fetchRows(spreadsheetId, range, connection).then(rows => console.log(rows));
-
 client.login(token);
+
+module.exports = {
+  fetchRows,
+  assignRoles,
+  extractNewEntries,
+  extractDiscordIDs
+};
--- index.test.js
+++ index.test.js
@@ -1,3 +1,24 @@
-it("is working", () => {
-  console.log("Yes");
+const { fetchRows, extractDiscordIDs } = require("./index");
+
+it("is returning rows", async () => {
+  const mockSheetsConnection = {
+    spreadsheets: {
+      values: {
+        get: obj =>
+          Promise.resolve({
+            data: { values: [["SomeUsername#6444"], ["AnotherUsername#3336"]] }
+          })
+      }
+    }
+  };
+  expect.assertions(1);
+  expect(
+    await fetchRows("ghdsj6Ghsakdf", "B2:B", mockSheetsConnection)
+  ).toEqual([["SomeUsername#6444"], ["AnotherUsername#3336"]]);
+});
+
+it("is extracting usernames from rows", () => {
+  expect(
+    extractDiscordIDs([["SomeUsername#6444"], ["AnotherUsername#3336"]])
+  ).toEqual(["SomeUsername#6444", "AnotherUsername#3336"]);
 });
